f48495167c594955f92770c5f258223c4c6806d2
braheezy <11700503+braheezy@users.noreply.github.com>
2024-08-18 17:04:01 -0700

    Code through "Cubemaps"

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"image"
 	_ "image/jpeg"
 	_ "image/png"
@@ -19,6 +18,7 @@ import (
 const (
 	initialWindowWidth  = 800
 	initialWindowHeight = 600
+	drawModel           = true
 )
 
 var (
@@ -35,68 +35,99 @@ var (
 
 	cubeVertices = []float32{
 		// positions          // texture Coords
-		-0.5, -0.5, -0.5, 0.0, 0.0,
-		0.5, -0.5, -0.5, 1.0, 0.0,
-		0.5, 0.5, -0.5, 1.0, 1.0,
-		0.5, 0.5, -0.5, 1.0, 1.0,
-		-0.5, 0.5, -0.5, 0.0, 1.0,
-		-0.5, -0.5, -0.5, 0.0, 0.0,
+		-0.5, -0.5, -0.5, 0.0, 0.0, -1.0,
+		0.5, -0.5, -0.5, 0.0, 0.0, -1.0,
+		0.5, 0.5, -0.5, 0.0, 0.0, -1.0,
+		0.5, 0.5, -0.5, 0.0, 0.0, -1.0,
+		-0.5, 0.5, -0.5, 0.0, 0.0, -1.0,
+		-0.5, -0.5, -0.5, 0.0, 0.0, -1.0,
 
-		-0.5, -0.5, 0.5, 0.0, 0.0,
-		0.5, -0.5, 0.5, 1.0, 0.0,
-		0.5, 0.5, 0.5, 1.0, 1.0,
-		0.5, 0.5, 0.5, 1.0, 1.0,
-		-0.5, 0.5, 0.5, 0.0, 1.0,
-		-0.5, -0.5, 0.5, 0.0, 0.0,
+		-0.5, -0.5, 0.5, 0.0, 0.0, 1.0,
+		0.5, -0.5, 0.5, 0.0, 0.0, 1.0,
+		0.5, 0.5, 0.5, 0.0, 0.0, 1.0,
+		0.5, 0.5, 0.5, 0.0, 0.0, 1.0,
+		-0.5, 0.5, 0.5, 0.0, 0.0, 1.0,
+		-0.5, -0.5, 0.5, 0.0, 0.0, 1.0,
 
-		-0.5, 0.5, 0.5, 1.0, 0.0,
-		-0.5, 0.5, -0.5, 1.0, 1.0,
-		-0.5, -0.5, -0.5, 0.0, 1.0,
-		-0.5, -0.5, -0.5, 0.0, 1.0,
-		-0.5, -0.5, 0.5, 0.0, 0.0,
-		-0.5, 0.5, 0.5, 1.0, 0.0,
+		-0.5, 0.5, 0.5, -1.0, 0.0, 0.0,
+		-0.5, 0.5, -0.5, -1.0, 0.0, 0.0,
+		-0.5, -0.5, -0.5, -1.0, 0.0, 0.0,
+		-0.5, -0.5, -0.5, -1.0, 0.0, 0.0,
+		-0.5, -0.5, 0.5, -1.0, 0.0, 0.0,
+		-0.5, 0.5, 0.5, -1.0, 0.0, 0.0,
 
-		0.5, 0.5, 0.5, 1.0, 0.0,
-		0.5, 0.5, -0.5, 1.0, 1.0,
-		0.5, -0.5, -0.5, 0.0, 1.0,
-		0.5, -0.5, -0.5, 0.0, 1.0,
-		0.5, -0.5, 0.5, 0.0, 0.0,
-		0.5, 0.5, 0.5, 1.0, 0.0,
+		0.5, 0.5, 0.5, 1.0, 0.0, 0.0,
+		0.5, 0.5, -0.5, 1.0, 0.0, 0.0,
+		0.5, -0.5, -0.5, 1.0, 0.0, 0.0,
+		0.5, -0.5, -0.5, 1.0, 0.0, 0.0,
+		0.5, -0.5, 0.5, 1.0, 0.0, 0.0,
+		0.5, 0.5, 0.5, 1.0, 0.0, 0.0,
 
-		-0.5, -0.5, -0.5, 0.0, 1.0,
-		0.5, -0.5, -0.5, 1.0, 1.0,
-		0.5, -0.5, 0.5, 1.0, 0.0,
-		0.5, -0.5, 0.5, 1.0, 0.0,
-		-0.5, -0.5, 0.5, 0.0, 0.0,
-		-0.5, -0.5, -0.5, 0.0, 1.0,
+		-0.5, -0.5, -0.5, 0.0, -1.0, 0.0,
+		0.5, -0.5, -0.5, 0.0, -1.0, 0.0,
+		0.5, -0.5, 0.5, 0.0, -1.0, 0.0,
+		0.5, -0.5, 0.5, 0.0, -1.0, 0.0,
+		-0.5, -0.5, 0.5, 0.0, -1.0, 0.0,
+		-0.5, -0.5, -0.5, 0.0, -1.0, 0.0,
 
-		-0.5, 0.5, -0.5, 0.0, 1.0,
-		0.5, 0.5, -0.5, 1.0, 1.0,
-		0.5, 0.5, 0.5, 1.0, 0.0,
-		0.5, 0.5, 0.5, 1.0, 0.0,
-		-0.5, 0.5, 0.5, 0.0, 0.0,
-		-0.5, 0.5, -0.5, 0.0, 1.0,
+		-0.5, 0.5, -0.5, 0.0, 1.0, 0.0,
+		0.5, 0.5, -0.5, 0.0, 1.0, 0.0,
+		0.5, 0.5, 0.5, 0.0, 1.0, 0.0,
+		0.5, 0.5, 0.5, 0.0, 1.0, 0.0,
+		-0.5, 0.5, 0.5, 0.0, 1.0, 0.0,
+		-0.5, 0.5, -0.5, 0.0, 1.0, 0.0,
 	}
-	planeVertices = []float32{
-		// positions          // texture Coords
-		5.0, -0.5, 5.0, 2.0, 0.0,
-		-5.0, -0.5, 5.0, 0.0, 0.0,
-		-5.0, -0.5, -5.0, 0.0, 2.0,
-
-		5.0, -0.5, 5.0, 2.0, 0.0,
-		-5.0, -0.5, -5.0, 0.0, 2.0,
-		5.0, -0.5, -5.0, 2.0, 2.0,
+	faces = []string{
+		"assets/skybox/right.jpg",
+		"assets/skybox/left.jpg",
+		"assets/skybox/top.jpg",
+		"assets/skybox/bottom.jpg",
+		"assets/skybox/front.jpg",
+		"assets/skybox/back.jpg",
 	}
-	quadVertices = []float32{
-		// vertex attributes for a quad that fills the entire screen in Normalized Device Coordinates.
-		// positions   // texCoords
-		-1.0, 1.0, 0.0, 1.0,
-		-1.0, -1.0, 0.0, 0.0,
-		1.0, -1.0, 1.0, 0.0,
+	skyboxVertices = []float32{
+		// positions
+		-1.0, 1.0, -1.0,
+		-1.0, -1.0, -1.0,
+		1.0, -1.0, -1.0,
+		1.0, -1.0, -1.0,
+		1.0, 1.0, -1.0,
+		-1.0, 1.0, -1.0,
 
-		-1.0, 1.0, 0.0, 1.0,
-		1.0, -1.0, 1.0, 0.0,
-		1.0, 1.0, 1.0, 1.0,
+		-1.0, -1.0, 1.0,
+		-1.0, -1.0, -1.0,
+		-1.0, 1.0, -1.0,
+		-1.0, 1.0, -1.0,
+		-1.0, 1.0, 1.0,
+		-1.0, -1.0, 1.0,
+
+		1.0, -1.0, -1.0,
+		1.0, -1.0, 1.0,
+		1.0, 1.0, 1.0,
+		1.0, 1.0, 1.0,
+		1.0, 1.0, -1.0,
+		1.0, -1.0, -1.0,
+
+		-1.0, -1.0, 1.0,
+		-1.0, 1.0, 1.0,
+		1.0, 1.0, 1.0,
+		1.0, 1.0, 1.0,
+		1.0, -1.0, 1.0,
+		-1.0, -1.0, 1.0,
+
+		-1.0, 1.0, -1.0,
+		1.0, 1.0, -1.0,
+		1.0, 1.0, 1.0,
+		1.0, 1.0, 1.0,
+		-1.0, 1.0, 1.0,
+		-1.0, 1.0, -1.0,
+
+		-1.0, -1.0, -1.0,
+		-1.0, -1.0, 1.0,
+		1.0, -1.0, -1.0,
+		1.0, -1.0, -1.0,
+		-1.0, -1.0, 1.0,
+		1.0, -1.0, 1.0,
 	}
 )
 
@@ -142,7 +173,7 @@ func main() {
 	// Listen to mouse events
 	window.SetCursorPosCallback(mouseCallback)
 	// Tell glfw to capture and hide the cursor
-	window.SetInputMode(glfw.CursorMode, glfw.CursorDisabled)
+	// window.SetInputMode(glfw.CursorMode, glfw.CursorDisabled)
 	// Listen to scroll events
 	window.SetScrollCallback(scrollCallback)
 
@@ -164,9 +195,17 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	screenShader, err := NewShader("shaders/screen.vs", "shaders/screen.fs")
+	skyboxShader, err := NewShader("shaders/skybox.vs", "shaders/skybox.fs")
 	if err != nil {
 		log.Fatal(err)
+	}
+
+	/*
+	 * load models
+	 */
+	customModel := Model{}
+	if drawModel {
+		customModel.LoadModel("assets/backpack/backpack.obj")
 	}
 
 	// cube VAO
@@ -177,70 +216,29 @@ func main() {
 	gl.BindBuffer(gl.ARRAY_BUFFER, cubeVBO)
 	gl.BufferData(gl.ARRAY_BUFFER, len(cubeVertices)*int(unsafe.Sizeof(cubeVertices[0])), gl.Ptr(cubeVertices), gl.STATIC_DRAW)
 	gl.EnableVertexAttribArray(0)
-	gl.VertexAttribPointer(0, 3, gl.FLOAT, false, int32(5*unsafe.Sizeof(float32(0))), gl.Ptr(nil))
+	gl.VertexAttribPointer(0, 3, gl.FLOAT, false, int32(6*unsafe.Sizeof(float32(0))), gl.Ptr(nil))
 	gl.EnableVertexAttribArray(1)
-	gl.VertexAttribPointer(1, 2, gl.FLOAT, false, int32(5*unsafe.Sizeof(float32(0))), gl.Ptr(3*unsafe.Sizeof(float32(0))))
-	gl.BindVertexArray(0)
-	// plane VAO
-	var planeVAO, planeVBO uint32
-	gl.GenVertexArrays(1, &planeVAO)
-	gl.GenBuffers(1, &planeVBO)
-	gl.BindVertexArray(planeVAO)
-	gl.BindBuffer(gl.ARRAY_BUFFER, planeVBO)
-	gl.BufferData(gl.ARRAY_BUFFER, len(planeVertices)*int(unsafe.Sizeof(planeVertices[0])), gl.Ptr(planeVertices), gl.STATIC_DRAW)
+	gl.VertexAttribPointer(1, 3, gl.FLOAT, false, int32(6*unsafe.Sizeof(float32(0))), gl.Ptr(3*unsafe.Sizeof(float32(0))))
+	// skybox VAO
+	var skyboxVAO, skyboxVBO uint32
+	gl.GenVertexArrays(1, &skyboxVAO)
+	gl.GenBuffers(1, &skyboxVBO)
+	gl.BindVertexArray(skyboxVAO)
+	gl.BindBuffer(gl.ARRAY_BUFFER, skyboxVBO)
+	gl.BufferData(gl.ARRAY_BUFFER, len(skyboxVertices)*int(unsafe.Sizeof(skyboxVertices[0])), gl.Ptr(skyboxVertices), gl.STATIC_DRAW)
 	gl.EnableVertexAttribArray(0)
-	gl.VertexAttribPointer(0, 3, gl.FLOAT, false, int32(5*unsafe.Sizeof(float32(0))), gl.Ptr(nil))
-	gl.EnableVertexAttribArray(1)
-	gl.VertexAttribPointer(1, 2, gl.FLOAT, false, int32(5*unsafe.Sizeof(float32(0))), gl.Ptr(3*unsafe.Sizeof(float32(0))))
-	gl.BindVertexArray(0)
-	// screen quad VAO
-	var quadVAO, quadVBO uint32
-	gl.GenVertexArrays(1, &quadVAO)
-	gl.GenBuffers(1, &quadVBO)
-	gl.BindVertexArray(quadVAO)
-	gl.BindBuffer(gl.ARRAY_BUFFER, quadVBO)
-	gl.BufferData(gl.ARRAY_BUFFER, len(quadVertices)*int(unsafe.Sizeof(quadVertices[0])), gl.Ptr(quadVertices), gl.STATIC_DRAW)
-	gl.EnableVertexAttribArray(0)
-	gl.VertexAttribPointer(0, 2, gl.FLOAT, false, int32(4*unsafe.Sizeof(float32(0))), gl.Ptr(nil))
-	gl.EnableVertexAttribArray(1)
-	gl.VertexAttribPointer(1, 2, gl.FLOAT, false, int32(4*unsafe.Sizeof(float32(0))), gl.Ptr(2*unsafe.Sizeof(float32(0))))
+	gl.VertexAttribPointer(0, 3, gl.FLOAT, false, int32(3*unsafe.Sizeof(float32(0))), gl.Ptr(nil))
 
 	// load textures
 	cubeTexture := loadTextures("assets/container.jpg")
-	floorTexture := loadTextures("assets/metal.png")
+	cubemapTexture := loadCubemap(faces)
 
 	// shader configuration
 	shaderProgram.use()
 	shaderProgram.setInt("texture1", 0)
 
-	screenShader.use()
-	screenShader.setInt("screenTexture", 0)
-
-	// framebuffer configuration
-	var framebuffer uint32
-	gl.GenFramebuffers(1, &framebuffer)
-	gl.BindFramebuffer(gl.FRAMEBUFFER, framebuffer)
-	// create a color attachment texture
-	var textureColorbuffer uint32
-	gl.GenTextures(1, &textureColorbuffer)
-	gl.BindTexture(gl.TEXTURE_2D, textureColorbuffer)
-	gl.TexImage2D(gl.TEXTURE_2D, 0, gl.RGB, initialWindowWidth, initialWindowHeight, 0, gl.RGB, gl.UNSIGNED_BYTE, gl.Ptr(nil))
-	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR)
-	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR)
-	gl.FramebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, textureColorbuffer, 0)
-	// create a renderbuffer object for depth and stencil attachment (we won't be sampling these)
-	var rbo uint32
-	gl.GenRenderbuffers(1, &rbo)
-	gl.BindRenderbuffer(gl.RENDERBUFFER, rbo)
-	// use a single renderbuffer object for both a depth AND stencil buffer.
-	gl.RenderbufferStorage(gl.RENDERBUFFER, gl.DEPTH24_STENCIL8, initialWindowWidth, initialWindowHeight)
-	// now actually attach it
-	gl.FramebufferRenderbuffer(gl.FRAMEBUFFER, gl.DEPTH_STENCIL_ATTACHMENT, gl.RENDERBUFFER, rbo)
-	// now that we actually created the framebuffer and added all attachments we want to check if it is actually complete now
-	if gl.CheckFramebufferStatus(gl.FRAMEBUFFER) != gl.FRAMEBUFFER_COMPLETE {
-		fmt.Printf("ERROR::FRAMEBUFFER:: Framebuffer is not complete!\n")
-	}
-	gl.BindFramebuffer(gl.FRAMEBUFFER, 0)
+	skyboxShader.use()
+	skyboxShader.setInt("skybox", 0)
 
 	// Run the render loop until the window is closed by the user.
 	for !window.ShouldClose() {
@@ -253,53 +251,45 @@ func main() {
 		processInput(window)
 
 		// render
-		// bind to framebuffer and draw scene as we normally would to color texture
-		gl.BindFramebuffer(gl.FRAMEBUFFER, framebuffer)
-		// enable depth testing (is disabled for rendering screen-space quad)
-		gl.Enable(gl.DEPTH_TEST)
-
-		// make sure we clear the framebuffer's content
 		gl.ClearColor(0.1, 0.1, 0.1, 1.0)
 		gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
 
 		shaderProgram.use()
-
+		model := mgl32.Ident4()
 		view := camera.getViewMatrix()
 		projection := mgl32.Perspective(mgl32.DegToRad(camera.zoom), initialWindowWidth/initialWindowHeight, 0.1, 100.0)
 		shaderProgram.setMat4("view", view)
 		shaderProgram.setMat4("projection", projection)
+		shaderProgram.setMat4("model", model)
+		shaderProgram.setVec3("cameraPos", camera.position)
 
-		// cubes
-		gl.BindVertexArray(cubeVAO)
+		if drawModel {
+			model = model.Mul4(mgl32.Translate3D(0.0, 0.0, 0.0))
+			model = model.Mul4(mgl32.Scale3D(1.0, 1.0, 1.0))
+			shaderProgram.setMat4("model", model)
+			customModel.Draw(*shaderProgram)
+		} else {
+			// cubes
+			gl.BindVertexArray(cubeVAO)
+			gl.ActiveTexture(gl.TEXTURE0)
+			gl.BindTexture(gl.TEXTURE_2D, cubeTexture)
+			gl.DrawArrays(gl.TRIANGLES, 0, 36)
+			gl.BindVertexArray(0)
+		}
+
+		// skybox last for efficiency
+		gl.DepthFunc(gl.LEQUAL)
+		skyboxShader.use()
+		view = extract3x3From4x4(camera.getViewMatrix())
+		skyboxShader.setMat4("view", view)
+		skyboxShader.setMat4("projection", projection)
+		// skybox cube
+		gl.BindVertexArray(skyboxVAO)
 		gl.ActiveTexture(gl.TEXTURE0)
-		gl.BindTexture(gl.TEXTURE_2D, cubeTexture)
-		model := mgl32.Ident4().Mul4(mgl32.Translate3D(-1.0, 0.0, -1.0))
-		shaderProgram.setMat4("model", model)
+		gl.BindTexture(gl.TEXTURE_CUBE_MAP, cubemapTexture)
 		gl.DrawArrays(gl.TRIANGLES, 0, 36)
-		model = mgl32.Ident4().Mul4(mgl32.Translate3D(2.0, 0.0, 0.0))
-		shaderProgram.setMat4("model", model)
-		gl.DrawArrays(gl.TRIANGLES, 0, 36)
-		// floor
-		gl.BindVertexArray(planeVAO)
-		gl.BindTexture(gl.TEXTURE_2D, floorTexture)
-		shaderProgram.setMat4("model", mgl32.Ident4())
-		gl.DrawArrays(gl.TRIANGLES, 0, 6)
 		gl.BindVertexArray(0)
-
-		// now bind back to default framebuffer and draw a quad plane with the attached framebuffer color texture
-		gl.BindFramebuffer(gl.FRAMEBUFFER, 0)
-		// disable depth test so screen-space quad isn't discarded due to depth test.
-		gl.Disable(gl.DEPTH_TEST)
-		// clear all relevant buffers
-		// set clear color to white (not really necessary actually, since we won't be able to see behind the quad anyways)
-		gl.ClearColor(1.0, 1.0, 1.0, 1.0)
-		gl.Clear(gl.COLOR_BUFFER_BIT)
-
-		screenShader.use()
-		gl.BindVertexArray(quadVAO)
-		// use the color attachment texture as the texture of the quad plane
-		gl.BindTexture(gl.TEXTURE_2D, textureColorbuffer)
-		gl.DrawArrays(gl.TRIANGLES, 0, 6)
+		gl.DepthFunc(gl.LESS)
 
 		// Swap the color buffer and poll events
 		window.SwapBuffers()
@@ -372,6 +362,49 @@ func loadTextures(filePath string) uint32 {
 	gl.GenTextures(1, &texture)
 	gl.BindTexture(gl.TEXTURE_2D, texture)
 
+	pixelData, format, width, height := loadPixels(filePath)
+
+	// Upload texture data to the GPU
+	gl.TexImage2D(gl.TEXTURE_2D, 0, format, int32(width), int32(height), 0, uint32(format), gl.UNSIGNED_BYTE, gl.Ptr(pixelData))
+
+	// Generate mipmaps
+	gl.GenerateMipmap(gl.TEXTURE_2D)
+
+	// Set texture wrapping/filtering options
+	// if format == gl.RGBA {
+	// 	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE)
+	// 	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE)
+	// } else {
+	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.REPEAT)
+	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.REPEAT)
+	// }
+	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR_MIPMAP_LINEAR)
+	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR)
+
+	return texture
+}
+
+func loadCubemap(faces []string) uint32 {
+	var textureID uint32
+	gl.GenTextures(1, &textureID)
+	gl.BindTexture(gl.TEXTURE_CUBE_MAP, textureID)
+
+	for i, face := range faces {
+		pixelData, format, width, height := loadPixels(face)
+		gl.TexImage2D(gl.TEXTURE_CUBE_MAP_POSITIVE_X+uint32(i), 0, format, int32(width), int32(height), 0, uint32(format), gl.UNSIGNED_BYTE, gl.Ptr(pixelData))
+	}
+
+	gl.TexParameteri(gl.TEXTURE_CUBE_MAP, gl.TEXTURE_MIN_FILTER, gl.LINEAR)
+	gl.TexParameteri(gl.TEXTURE_CUBE_MAP, gl.TEXTURE_MAG_FILTER, gl.LINEAR)
+	gl.TexParameteri(gl.TEXTURE_CUBE_MAP, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE)
+	gl.TexParameteri(gl.TEXTURE_CUBE_MAP, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE)
+	gl.TexParameteri(gl.TEXTURE_CUBE_MAP, gl.TEXTURE_WRAP_R, gl.CLAMP_TO_EDGE)
+
+	return textureID
+}
+
+func loadPixels(filePath string) ([]byte, int32, int, int) {
+
 	// Open and decode the texture image file
 	textureFile, err := os.Open(filePath)
 	if err != nil {
@@ -379,7 +412,6 @@ func loadTextures(filePath string) uint32 {
 	}
 	defer textureFile.Close()
 
-	// Decode the image (JPEG, PNG, etc.)
 	textureImage, _, err := image.Decode(textureFile)
 	if err != nil {
 		log.Fatalf("Failed to decode texture file [%s]: %v", filePath, err)
@@ -388,16 +420,15 @@ func loadTextures(filePath string) uint32 {
 	// Determine the number of components and the OpenGL format
 	bounds := textureImage.Bounds()
 	width, height := bounds.Dx(), bounds.Dy()
-	var format, internalFormat int32
+	var format int32
 	var pixelData []byte
 
 	switch img := textureImage.(type) {
 	case *image.Gray:
 		format = gl.RED
-		internalFormat = gl.RED
 		pixelData = make([]byte, width*height)
 		index := 0
-		for y := bounds.Max.Y - 1; y >= bounds.Min.Y; y-- {
+		for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
 			for x := bounds.Min.X; x < bounds.Max.X; x++ {
 				r := img.GrayAt(x, y).Y
 				pixelData[index] = r
@@ -422,10 +453,9 @@ func loadTextures(filePath string) uint32 {
 
 		if hasAlpha {
 			format = gl.RGBA
-			internalFormat = gl.RGBA
 			pixelData = make([]byte, width*height*4)
 			index := 0
-			for y := bounds.Max.Y - 1; y >= bounds.Min.Y; y-- {
+			for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
 				for x := bounds.Min.X; x < bounds.Max.X; x++ {
 					r, g, b, a := img.At(x, y).RGBA()
 					pixelData[index] = byte(r >> 8)
@@ -438,10 +468,9 @@ func loadTextures(filePath string) uint32 {
 		} else {
 			// Treat as RGB if alpha is not used
 			format = gl.RGB
-			internalFormat = gl.RGB
 			pixelData = make([]byte, width*height*3)
 			index := 0
-			for y := bounds.Max.Y - 1; y >= bounds.Min.Y; y-- {
+			for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
 				for x := bounds.Min.X; x < bounds.Max.X; x++ {
 					r, g, b, _ := img.At(x, y).RGBA()
 					pixelData[index] = byte(r >> 8)
@@ -453,10 +482,9 @@ func loadTextures(filePath string) uint32 {
 		}
 	case *image.NRGBA:
 		format = gl.RGBA
-		internalFormat = gl.RGBA
 		pixelData = make([]byte, width*height*4)
 		index := 0
-		for y := bounds.Max.Y - 1; y >= bounds.Min.Y; y-- {
+		for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
 			for x := bounds.Min.X; x < bounds.Max.X; x++ {
 				r, g, b, a := img.At(x, y).RGBA()
 				pixelData[index] = byte(r >> 8)
@@ -469,10 +497,9 @@ func loadTextures(filePath string) uint32 {
 	default:
 		// Handle RGB images without alpha channel
 		format = gl.RGB
-		internalFormat = gl.RGB
 		pixelData = make([]byte, width*height*3)
 		index := 0
-		for y := bounds.Max.Y - 1; y >= bounds.Min.Y; y-- {
+		for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
 			for x := bounds.Min.X; x < bounds.Max.X; x++ {
 				r, g, b, _ := textureImage.At(x, y).RGBA()
 				pixelData[index] = byte(r >> 8)
@@ -483,22 +510,14 @@ func loadTextures(filePath string) uint32 {
 		}
 	}
 
-	// Upload texture data to the GPU
-	gl.TexImage2D(gl.TEXTURE_2D, 0, internalFormat, int32(width), int32(height), 0, uint32(format), gl.UNSIGNED_BYTE, gl.Ptr(pixelData))
+	return pixelData, format, width, height
+}
 
-	// Generate mipmaps
-	gl.GenerateMipmap(gl.TEXTURE_2D)
-
-	// Set texture wrapping/filtering options
-	// if format == gl.RGBA {
-	// 	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE)
-	// 	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE)
-	// } else {
-	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.REPEAT)
-	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.REPEAT)
-	// }
-	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR_MIPMAP_LINEAR)
-	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR)
-
-	return texture
+func extract3x3From4x4(mat mgl32.Mat4) mgl32.Mat4 {
+	return mgl32.Mat4{
+		mat[0], mat[1], mat[2], 0, // First column
+		mat[4], mat[5], mat[6], 0, // Second column
+		mat[8], mat[9], mat[10], 0, // Third column
+		0, 0, 0, // Fourth column
+	}
 }
